bbfaa4a3b5349aa6ec89a43fcd6dd4c8bec96d85
schhunheng <schhunheng.it@gmail.com>
2020-07-28 18:30:38 +0700

    ass2

--- php/includes/productdetail.php
+++ php/includes/productdetail.php
@@ -1,7 +1,61 @@
-
-<?php
-echo "<div class=\"listproduct\">";
-        
-echo "</div>"
-?>   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include "meta.php" ?>
+  <link rel="stylesheet" href="../../css/productdetail.css">
+</head>
+<body>
+    <div class="productimage">
+        <div class="images">
+            <span id="discount">20%</span>
+            <img src="../../asset/img/productionList/clothes.png" id="img">
+            <div class="smallimages">
+                <img src="../../asset/img/productionList/clothes.png" >
+                <img src="../../asset/img/productionList/clothes.png">
+                <img src="../../asset/img/productionList/clothes.png">
+            </div>
+        </div>
+        <div class="productdetailsection">
+            <h2>White T-shirt -Nike</h2>
+            <span id="originalvalues">15$<span id="discountvalues">$12</span></span>
+            <p id="description">Nike Women Pink Half Zip Lightweight Hooded Shirt Top Athietic Stretch Size M</p>
+            <button id="btnbook"><img style="width:24px ;padding: 0px 5px;margin:0px " src="../../asset/img/buttons/btnbook.png">Cart</button>
+            <div class="producttag">
+                <span>Male T-Shirt</span>
+                <span>Single Color</span>
+            </div>
+        </div>
+    </div>
+    <div class="reviewsection">
+        <h2>Comments</h2>
+        <div class="comments">
+            <div class="profile">
+                <img src="" alt="">
+                <div class="textprofile">
+                  <b style="color:darkblue;">Anonymous</b>
+                  <p id="time"></p>
+                </div>
+            <div class="descriptioncomment">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Similique minima commodi, veniam animi perspiciatis neque molestiae 
+                 adipisci asperiores aperiam a! Odio dignissimos necessitatibus, magni consectetur
+                  totam aperiam sapiente nesciunt praesentium.
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="writecomment">
+       <form action="">
+          <fieldset>
+             <legend>Write your review here</legend>
+             <textarea name="" id="textarea"></textarea>
+               <div class="buttons">
+                  <button type="submit">Send</button>
+                  <button type="reset">Discard</button>
+               </div>
+          </fieldset>
+       </form>
+    </div>
+</body>
+</html>  
     
